b640154485ae07b5f9bfced3a89c02e93b3aad8b
josuecorreasilva <josuecorreasilva@gmail.com>
2026-09-12 22:50:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ with col_logo1:
 
 with col_logo2:
     # IMPORTANTE: Coloque o nome exato da nova imagem que você fará upload no GitHub
-    st.image("horizontalloggoverr.png", width=280)
+    st.image("horizontalloggoverr.png", width=350)
 
 aba1, aba2 = st.tabs(["Fazer Novo Pedido", "Painel Gerencial (Pedidos Salvos)"])
 
