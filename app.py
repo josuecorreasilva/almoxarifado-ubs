56fc92e302b8df8d21e77b9406c86da28e75728a
josuecorreasilva <josuecorreasilva@gmail.com>
2026-09-18 13:42:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -222,14 +222,14 @@ with aba1:
             val_raw = item_row[col_preco].values[0]
             
             try:
-            if isinstance(val_raw, str):
-            # Remove R$, espaços e ajusta o formato brasileiro (vírgula para ponto)
-            val_limpo = val_raw.replace("R$", "").strip().replace(".", "").replace(",", ".")
-            valor_unitario_atual = float(val_limpo)
-        else:
-            valor_unitario_atual = float(val_raw)
-    except:
-        valor_unitario_atual = 0.0
+                if isinstance(val_raw, str):
+                    # Remove R$, espaços e ajusta o formato brasileiro (vírgula para ponto)
+                    val_limpo = val_raw.replace("R$", "").strip().replace(".", "").replace(",", ".")
+                    valor_unitario_atual = float(val_limpo)
+                else:
+                    valor_unitario_atual = float(val_raw)
+            except:
+                valor_unitario_atual = 0.0
         
     if st.button("➕ Adicionar Item ao Pedido", key="btn_adicionar_item"):
         subtotal = quantidade * valor_unitario_atual
